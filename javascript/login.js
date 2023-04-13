dc293119c9a45096ab73229f8e4746a5db4dceb6
kauawb02 <kauabatista254@gmail.com>
2023-04-13 18:14:01 -0300

    Feat: botão de login e criar conta adicionados

--- javascript/login.js
+++ javascript/login.js
@@ -1,22 +1,33 @@
 function validaEmail() {
   const email = document.getElementById('email');
+  const alert = document.getElementById('alert');
   if (
     !email.value.includes('@') ||
     !email.value.includes('.com') ||
     email.value.length < 10
   ) {
     email.style.border = '3px solid red';
+    alert.style.display = 'block';
+    alert.innerHTML = 'Informe um E-mail valido!';
   } else {
     email.style.border = 'none';
+    alert.style.display = 'none';
+    email.style.border = '';
+
   }
 }
 
 function validaPassword() {
   const password = document.getElementById('password');
-  console.log(password.value.length <= 5)
+  const alert = document.getElementById('alert');
+
   if (password.value.length <= 5) {
+    alert.style.display = 'block';
+    alert.innerHTML = 'Informe uma SENHA maior que 5 dígitos!';
     password.style.border = '3px solid red';
   } else {
     password.style.border = 'none';
+    alert.style.display = 'none';
+    email.style.border = '';
   }
 }
